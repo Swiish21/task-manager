6e11380a99404ef3a3573b6e98c8eda4a6ed6e0f
Swiiish21 <malcolmotieno@gmail.com>
2024-08-13 09:56:03 +0300

    minor changes

---
+++ script.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDYtN8JIcKO3eKb72vPksf1BsjYVizkH9g",
+    authDomain: "task-manager-d385f.firebaseapp.com",
+    databaseURL: "https://task-manager-d385f-default-rtdb.firebaseio.com",
+    projectId: "task-manager-d385f",
+    storageBucket: "task-manager-d385f.appspot.com",
+    messagingSenderId: "802490153269",
+    appId: "1:802490153269:web:b027cedac006110b0c347c",
+    measurementId: "G-H5CSEV6JMF"
+  };
