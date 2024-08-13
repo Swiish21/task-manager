a4be044afde83f34cd05abd7fb9fe48c02b8fa04
Swiiish21 <malcolmotieno@gmail.com>
2024-08-13 12:47:20 +0300

    ...

--- script.js
+++ script.js
@@ -10,5 +10,73 @@ const firebaseConfig = {
     measurementId: "G-H5CSEV6JMF"
   };
 
-  // Initialize Firebase
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+var totalItems
+var maxCode
+var code
+
+function storeTask(event) {
+    event.preventDefault();
+
+    //get data entered by user
+    var task = document.getElementById("task").value;
+    var desc = document.getElementById("desc").value;
+    document.getElementById("task").value = "";
+    document.getElementById("desc").value = "";
+    
+    code = totalItems
+    if (totalItems < maxCode) {
+        code = maxCode + 1
+    }
+
+    firebase.database().ref("Tasklist" + code).set({
+        taskName: task,
+        descName: desc,
+        status : "Pending",
+    })
+
+    //update total items
+    firebase
+        .database()
+        .ref("totalItems")
+        .update({
+            totalItems: totalItems + 1,
+            maxCode: maxCode + 1
+        });
+    
+    if (document.getElementById("info") != null) {
+        document.getElementById("info").remove();
+    }
+
+    // Show the data in the body in form of card
+  document.getElementById("tasks-header").insertAdjacentHTML(
+    "afterend",
+    `<div class="Task-item" id="${code}">
+    <div class="data" id="${task}">
+        <button id="done" class="done" onclick = "changeStatus('${code}')"><i class="far fa-check-circle"></i></button>
+        <h2 class="Task">${task}</h2>
+        <p class="desc">${desc}</p>
+        <small id = "status"></small>
+    </div>
+    <hr>
+    <div class="buttons">
+        <button class="button edit" id="editbtn" onclick = "editData('${code}')"><i class="fas fa-edit"></i> EDIT TASK</button>
+        <button class="button delete" id="deletebtn" onclick = "deleteData('${code}')">
+        <i class="fas fa-trash-alt"></i>DELETE TASK</button>
+    </div>
+    
+    </div>`
+  );
+}
+
+var data;
+firebase
+  .database()
+  .ref("TaskList")
+  .on("value", function (snapshot) {
+    data = snapshot.val();
+    console.log("This is data speaking from open");
+    console.log(data);
+  });
